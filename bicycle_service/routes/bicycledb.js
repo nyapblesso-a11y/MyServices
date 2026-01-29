a45119970d0bd30783dfee8e2191bfe88a92d8a5
nyapblesso-a11y <nyapblesso@gmail.com>
2026-01-29 14:07:52 +0100

    the bicycle database has benn implemented and with the various methods acquire mose especially the get method nad

--- bicycle_service/routes/bicycledb.js
+++ bicycle_service/routes/bicycledb.js
@@ -1,12 +1,17 @@
-import express from 'express'
-import {STATUS_CODES} from 'node:http'
-import db from '../config/db.js'
-import url from 'node:url'
-const router = express.Router()
+import express from "express";
+import { STATUS_CODES } from "node:http";
+import bicycle_database from "../config/db.js";
+
+const router = express.Router();
 
 const validateContentType = (req, res, next) => {
-  if (['POST', 'PUT', 'PATCH'].includes(req.method) && !req.is('application/json')) {
-    return res.status(415).json({ error: 'Content-Type must be application/json' });
+  if (
+    ["POST", "PUT", "PATCH"].includes(req.method) &&
+    !req.is("application/json")
+  ) {
+    return res
+      .status(415)
+      .json({ error: "Content-Type must be application/json" });
   }
   next();
 };
@@ -14,15 +19,64 @@ const validateContentType = (req, res, next) => {
 router.use(validateContentType);
 
 const validataBicycleData = (data, isPartial = false) => {
- const error = []
- 
-if(!isPartial && !data.color) {
-    error.push('color is required')
-}
+  const error = [];
 
-if(data.color !== undefined && (typeof data.color !== 'string' || data.color.trim() === '' )) {
-    error.push('color must be a non-empty string')
-}
+  if (!isPartial && !data.color) {
+    error.push("color is required");
+  }
 
-return error
-}
+  if (
+    data.color !== undefined &&
+    (typeof data.color !== "string" || data.color.trim() === "")
+  ) {
+    error.push("color must be a non-empty string");
+  }
+
+  return error;
+};
+
+router.get("/", async function (req, res, next) {
+  try {
+    let query = "SELECT * FROM bicyclesdb";
+    const params = [];
+    const conditions = [];
+
+    if (color) {
+      conditions.push("color=?")
+      params.push(color)
+    }
+
+    if(conditions.length > 0) {
+        query += 'WHERE' + conditions.join('AND')
+    }
+
+    const results = bicycle_database.prepare(query).all(...params)
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async function (req, res, next) {
+    const id = req.params.id
+
+    try {
+    const results = bicycle_database.prepare("SELECT * FROM bicyclesdb WHERE id = ?").get(id)
+    if(!results) {
+        let error = new Error(STATUS_CODES[404])
+        error.status = 404
+        throw error
+    }
+   const bicycle = {
+    ...results, _links: {
+        self: {href: `/bicycle/${results.id}`},
+        collection: { href: '/bicycle'}
+    }
+   }
+   res.json(bicycle)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+export default router
