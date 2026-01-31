487452c51622a7ac798b7c059f012a0579c19d0d
nyapblesso-a11y <nyapblesso@gmail.com>
2026-01-31 11:23:32 +0100

    the consumer service has benn implemented and then api requests can be made now

--- consumer_service/routes/index.js
+++ consumer_service/routes/index.js
@@ -50,6 +50,45 @@ const { BICYCLE_SERVICE_PORT = 5000, BRAND_SERVICE_PORT = 3030 } = process.env;
 const bicycleService = `http://localhost:${BICYCLE_SERVICE_PORT}`;
 const brandService = `http://localhost:${BRAND_SERVICE_PORT}`;
 
+router.get("/", async function (req, res, next) {
+  const noop = Function.prototype;
+  const bicycleReq = await fetch(`${bicycleService}/bicycle/`);
+  const brandReq = await fetch(`${brandService}/brand/`);
+
+  if (bicycleReq.status === 404 || brandReq.status === 404) {
+    next(createError(404, "Bicycle or Brand not found"));
+    return;
+  }
+
+  if (bicycleReq.status === 400 || brandReq.status === 400) {
+    next(createError(400, "Bad Request to Bicycle or Brand service"));
+    return;
+  }
+
+  const bicycleProm = bicycleReq.json();
+  const brandProm = brandReq.json();
+
+  bicycleProm.catch(noop);
+
+  const results = await Promise.allSettled([bicycleProm, brandProm]);
+  // res.send(results);
+
+  const bicycles = results[0].value.data;
+  const brands = results[1].value.data;
+
+  const combined = bicycles.map((bike) => {
+    const brandMatch = brands.find((b) => b.id === bike.id);
+
+    return {
+      id: bike.id,
+      color: bike.color,
+      brand: brandMatch ? brandMatch.brand : null,
+    };
+  });
+
+res.send(combined);
+});
+
 router.get("/:id", async function (req, res, next) {
   const { id } = req.params;
 
@@ -59,12 +98,12 @@ router.get("/:id", async function (req, res, next) {
 
   if (bicycleReq.status === 404 || brandReq.status === 404) {
     next(createError(404, "Bicycle or Brand not found"));
-    return
+    return;
   }
 
   if (bicycleReq.status === 400 || brandReq.status === 400) {
     next(createError(400, "Bad Request to Bicycle or Brand service"));
-    return
+    return;
   }
 
   const bicycleProm = bicycleReq.json();
@@ -72,33 +111,31 @@ router.get("/:id", async function (req, res, next) {
 
   bicycleProm.catch(noop);
 
-  
-  
   const results = await Promise.allSettled([bicycleProm, brandProm]);
 
-  const mapped = results.map(item => {
-  if (item.value.color) {
-    return {
-      id: item.value.id,
-      color: item.value.color
-    };
-  }
+  const mapped = results.map((item) => {
+    if (item.value.color) {
+      return {
+        id: item.value.id,
+        color: item.value.color,
+      };
+    }
 
-  if (item.value.brand) {
-    return {
-      brand: item.value.brand
-    };
-  }
-});
+    if (item.value.brand) {
+      return {
+        brand: item.value.brand,
+      };
+    }
+  });
 
-const combined = Object.assign({}, ...mapped);
+  const combined = Object.assign({}, ...mapped);
 
- res.send(combined)
+  res.send(combined);
 
-// await Promise.all(
-//   ...combined.map((brand, color)=> consume_database.exec(`INSERT INTO consumer (brand), (color) VALUE (?, ?)`,[brand.brand ,color.color]) )
-// )
-// res.send({message: 'Data add successfully into the database'}, combined)
+  // await Promise.all(
+  //   ...combined.map((brand, color)=> consume_database.exec(`INSERT INTO consumer (brand), (color) VALUE (?, ?)`,[brand.brand ,color.color]) )
+  // )
+  // res.send({message: 'Data add successfully into the database'}, combined)
 });
 
 export default router;
